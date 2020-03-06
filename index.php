2a521acd79abbac08d749d41dc3b0df483f72ee1
webmusica <webprimus1@gmail.com>
2020-03-06 12:30:11 -0300

    test 1 Al Master

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="initial-scale=1">
+	<title>Proyecto Test</title>
+	<!-- estilos css-->
+	<link rel="stylesheet" href="css/css.css">
+	<!-- Css Bootstrap -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <!-- Js Bootstrap -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+	<!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+
+</head>
+<body>
+	<header>
+      
+		<nav class="navbar navbar-dark">
+		  <div class="container-fluid">
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+		        <span class="fa fa-bars"></span>
+		      </button>
+		      <a class="navbar-brand" href="#"></a>
+		    </div>
+		    <div class="collapse navbar-collapse" id="myNavbar">
+		      <ul class="nav navbar-nav">
+		        <li class="active"><a href="#"></a></li>
+		        <li><a href="#"> 1</a></li>
+		        <li><a href="#"> 2</a></li>
+		        <li><a href="#"> 3</a></li>
+		      </ul>
+		      <ul class="nav navbar-nav navbar-right">
+		        <li><a href="#"><span class="fa fa-user"></span> Inicio </a></li>
+		        <li><a href="#"><span class="fa fa sign-in"></span></a></li>
+		      </ul>
+		    </div>
+		  </div>
+		</nav>
+
+	</header>
+
+<footer>
+	<div>
+		
+	</div>
+</footer>
+</body>
+</html>
